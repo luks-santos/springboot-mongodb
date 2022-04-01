179ff8a72096b761bdffe868f845d9c91fead0d4
Lucas Batista <lucas.bt46@gmail.com>
2022-04-01 18:13:43 -0300

    primeiro commit

---
+++ src/test/java/com/punkmetal/workshopmongodb/WorkshopmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.punkmetal.workshopmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
