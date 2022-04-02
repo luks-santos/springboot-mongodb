7f9003e2fd95af6afe918078b976c1e8ebfb9eea
Lucas Batista <lucas.bt46@gmail.com>
2022-04-02 11:28:35 -0300

    Entity User e REST funcionando

---
+++ src/main/java/com/punkmetal/workshopmongodb/resources/UserResources.java
@@ -0,0 +1,27 @@
+package com.punkmetal.workshopmongodb.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.punkmetal.workshopmongodb.domain.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	//Ou @GetMapping
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = new ArrayList<>();
+		User maria = new User("1001", "Maria Brown", "maria@gmail.com");
+		User alex = new User("1002", "Alex Green", "alex@gmail.com");
+		list.addAll(Arrays.asList(maria, alex));
+		return ResponseEntity.ok().body(list);
+	}
+}
