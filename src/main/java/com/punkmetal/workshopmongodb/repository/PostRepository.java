3899a50409f6e3050d9b03e7f7fb9df1a299cc6b
Lucas Batista <lucas.bt46@gmail.com>
2022-04-05 09:24:20 -0300

    Consulta simples com @Query

--- src/main/java/com/punkmetal/workshopmongodb/repository/PostRepository.java
+++ src/main/java/com/punkmetal/workshopmongodb/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.punkmetal.workshopmongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.punkmetal.workshopmongodb.domain.Post;
@@ -11,4 +12,8 @@ import com.punkmetal.workshopmongodb.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String>{
 	
 	List<Post> findByTitleContainingIgnoreCase(String title);
+	
+	//Consulta o post pelo título com o primeiro parâmetro passado na função com ignore case sensitive
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> findByTitle(String title);
 }
--- src/main/java/com/punkmetal/workshopmongodb/services/PostService.java
+++ src/main/java/com/punkmetal/workshopmongodb/services/PostService.java
@@ -20,6 +20,7 @@ public class PostService {
 	}
 	
 	public List<Post> findByTitle(String title) {
-		return repository.findByTitleContainingIgnoreCase(title);
+		return repository.findByTitle(title);
+		//return repository.findByTitleContainingIgnoreCase(title);
 	}
 }
